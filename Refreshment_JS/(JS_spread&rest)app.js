17f61183b80c6de2d8d3c91ad94cea1de3b2fab9
jon-zer-1113 <114537615+jon-zer-1113@users.noreply.github.com>
2023-04-27 10:54:19 +0200

    commit 000

---
+++ Refreshment_JS/(JS_spread&rest)app.js
@@ -0,0 +1,49 @@
+// Spread (OPERATOR)
+
+const arr = [1,2,3];
+console.log(...arr);
+/* Ici, la méthode console.log() affichera 1 2 3 dans la console, 
+car les éléments du tableau arr ont été étendus en tant qu'arguments distincts pour la méthode console.log(). 
+C'est équivalent à appeler console.log(1, 2, 3). */
+
+const arr = [1,2,3];
+const arr2 = [arr, 4];
+console.log(arr2);
+/* ici j'utilise un deuxieme tableau dans lequel je mets le premier... SAUF QUE ça m'affiche [Array(3), 4] ...
+ET JUSTEMENT c'est là que le Spread va entrer en jeu : car il va faire appelle à TOUS les elements du 1er tableau*/
+
+const arr = [1,2,3];
+const arr2 = [...arr, 4];
+console.log(arr2);
+// Du coup là j'ai enfin tous les elements des 2 tableaux, dans ma console: [1, 2, 3, 4]
+
+const myObj = {
+    a: 1,
+    b: 2,
+    c: 3
+}
+const myObj2 = {
+    ...myObj,
+    d: 3
+}
+console.log(myObj2);
+// Ici la même chose avec un OBJET, on voit que Spread est très utile...
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// Rest (OPERATOR)
+
+function add (...args){
+    console.log(args);
+}
+console.log(add('a',2));
+/* Ici, on ne définit pas de paramètres en avance (nombre, type, etc...) MAIS juste args.
+Cela va nous permettre de "choisir" nos paramètres LORSQUE l'on appelle la fonction justement... */
+
+function add (a, b, ...args){
+    console.log(args);
+}
+console.log(add(1,2,5,6,7));
+// Ici on définit des paramètres MAIS on utilise seulement le reste des paramètres que l'on a défini avec "rest"(...args), donc ça affichera [5, 6, 7]
---
+++ Refreshment_JS/(JS_variables)app.js
@@ -0,0 +1,63 @@
+// Scope:
+
+function foo(){
+    if(true){
+        var x = 5;
+    }
+    console.log(x);
+}
+foo();
+
+
+
+// Redeclaration:
+
+var x = 5;
+x = 10;
+console.log(x);
+
+
+
+// Hoisting: (Le "hissage", on peut utiliser une fonction avant même de la déclarer (JS fait le lien et la détécte quand même...))
+
+foo();
+function foo(){
+    console.log("Hello");
+}
+
+// Hoisting des variables: (Avec les var cela ne fonctionne pas pour la valeur) 
+// PAR CONTRE: avec let et const cela ne fonctionne pas du tout
+
+console.log(x);
+var x = 10;
+
+
+
+// Global: (une var est stocké dans "window"... TANDIS que let et const : NON, cela évite les conflits)
+
+var maVarX = 10;
+console.log(maVarX);
+
+
+
+// Différence basique:
+
+var x = 10;
+x = "abc";
+console.log(x);
+// Ici la valeur de la variable est carrément changer en string
+
+const x = 10;
+x = "abc";
+console.log(x);
+// Ici, ça affiche une erreur car une constante est inchangeable... du moins...
+
+const x = {a: 5};
+x = {b: 5};
+console.log(x);
+// La même chose, une erreur en voulant changeant l'objet (déclaré entre {})
+
+const x = {a: 5};
+x.a = 10;
+console.log(x);
+// LOL, ici, par contre on peut changer l'intérieur de l'obje (car cela change JUSTE la valeur et non la variable stockée en mémoire)...
